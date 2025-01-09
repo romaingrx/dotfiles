5c6221616c2c86c55b57a5888294900fb050f147
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-01-09 10:11:41 +0100

    Refactor tmux configuration to enhance usability and streamline shortcuts
    - Removed the 'better-mouse-mode' plugin from the tmux configuration.
    - Added new key bindings to improve window and pane management, allowing for easier navigation in the current directory.
    - Introduced a shortcut to clear the screen using Ctrl+L for better user experience.

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -3,8 +3,6 @@
   shell = "${pkgs.zsh}/bin/zsh";
   terminal = "tmux-256color";
   plugins = with pkgs; [
-    tmuxPlugins.better-mouse-mode
-    # tmuxPlugins.vim-tmux-navigator
     {
       plugin = tmuxPlugins.catppuccin;
       extraConfig = '' 
@@ -31,5 +29,15 @@
     }
   ];
   extraConfig = ''
+    # Open new windows/panes in the current directory
+    bind '"' split-window -c "#{pane_current_path}"
+    bind % split-window -h -c "#{pane_current_path}"
+    bind c new-window -c "#{pane_current_path}"
+
+    # Add shortcut to change base directory for new windows
+    bind C attach-session -c "#{pane_current_path}" \; display-message "New working directory: #{pane_current_path}"
+
+    # Allow ctrl+L to clear screen
+    bind -n C-l send-keys C-l \; clear-history
   '';
 }
