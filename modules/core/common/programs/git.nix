a0f9cfe87f1cf7f064470f49908d73142cad6b89
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-02-25 16:34:47 +0100

    feat: Add Git aliases for improved workflow
        Enhance Git configuration with useful aliases:
        - Quick add all changes with 'a'
        - Simplified checkout commands 'co' and 'cob'
        - Improved branch listing with 'br'
        - Enhanced log view with 'lg'

--- modules/core/common/programs/git.nix
+++ modules/core/common/programs/git.nix
@@ -9,6 +9,16 @@
     signByDefault = true;
   };
 
+  aliases = {
+    a = "add .";
+    co = "checkout";
+    cob = "checkout -b";
+    br =
+      "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+    lg = ''
+      log --pretty=format:"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]" --abbrev-commit -30'';
+  };
+
   extraConfig = {
     commit.gpgsign = true;
     safe.directory = "*";
