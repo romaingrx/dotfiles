aa08ae504ed555591dec3ecd43c385bd87d9aacb
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-03-26 14:40:31 +0100

    feat: Add kubeseal to common package list
    Include kubeseal in the default.nix package list to enhance Kubernetes secret management capabilities.

--- modules/core/common/packages/default.nix
+++ modules/core/common/packages/default.nix
@@ -18,6 +18,7 @@
     minikube # When starting minikube with qemu, we need to specify the path as :  --qemu-firmware-path=/run/current-system/.../edk2-aarch64-code.fd
     kubectl # Kubectl should be a dependency of minikube so might not be needed
     kustomize
+    kubeseal
     qemu # Used as the hypervisor for minikube
     pre-commit
     nodejs_23
