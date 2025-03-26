71e7fb9126c13e1fbedb7c14ddb420a0ccdbad89
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-03-26 14:31:54 +0100

    feat: Add kustomize to common package list
    Include kustomize in the default.nix package list to enhance Kubernetes resource management capabilities.

--- modules/core/common/packages/default.nix
+++ modules/core/common/packages/default.nix
@@ -17,6 +17,7 @@
     git-lfs
     minikube # When starting minikube with qemu, we need to specify the path as :  --qemu-firmware-path=/run/current-system/.../edk2-aarch64-code.fd
     kubectl # Kubectl should be a dependency of minikube so might not be needed
+    kustomize
     qemu # Used as the hypervisor for minikube
     pre-commit
     nodejs_23
