da34cdc214cac7e68192c4f435f9250fba929e19
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-10-02 15:12:45 +0200

    feat: enhance tmux-navigator configuration in nixvim
    - Added settings to tmux-navigator for improved functionality, including disabling when zoomed and allowing mappings.
    - Implemented an autocommand to override tmux-navigator mappings in terminal buffers, enabling Ctrl+L to clear the screen.

--- modules/nixvim/config/utils/tmux-navigator.nix
+++ modules/nixvim/config/utils/tmux-navigator.nix
@@ -6,6 +6,23 @@
   config = lib.mkIf config.tmux-navigator.enable {
     plugins.tmux-navigator = {
       enable = true;
+      settings = {
+        disable_when_zoomed = true;
+        no_mappings = false;
+      };
     };
+
+    # Override tmux-navigator mappings in terminal buffers to allow Ctrl+L to work
+    extraConfigLua = ''
+      -- Disable tmux-navigator in terminal buffers so Ctrl+L can clear screen
+      vim.api.nvim_create_autocmd("TermOpen", {
+        callback = function()
+          vim.keymap.set("t", "<C-h>", "<C-h>", { buffer = true })
+          vim.keymap.set("t", "<C-j>", "<C-j>", { buffer = true })
+          vim.keymap.set("t", "<C-k>", "<C-k>", { buffer = true })
+          vim.keymap.set("t", "<C-l>", "<C-l>", { buffer = true })
+        end,
+      })
+    '';
   };
 }
