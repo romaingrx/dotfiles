2d8701b2ef83628ac1c9a07b0db9585e41eb5ee2
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-11-07 13:56:31 +0100

    deps: update flake.lock and configure treesitter-context settings
    This commit updates the `flake.lock` file with the latest revisions of
    dependencies including fenix, home-manager, nix-darwin, nixpkgs, nixvim,
    pre-commit-hooks.nix, and rust-analyzer. Additionally, it configures
    detailed settings for the treesitter-context plugin in nixvim and
    removes the empty `consumers` attribute from the neotest configuration.

--- modules/nixvim/config/languages/treesitter-nvim.nix
+++ modules/nixvim/config/languages/treesitter-nvim.nix
@@ -116,6 +116,17 @@
 
     plugins.treesitter-context = {
       enable = true;
+      settings = {
+        enable = true;
+        max_lines = 0;
+        min_window_height = 0;
+        line_numbers = true;
+        multiline_threshold = 20;
+        trim_scope = "outer";
+        mode = "cursor";
+        separator = null;
+        zindex = 20;
+      };
     };
 
     plugins.ts-context-commentstring = {
--- modules/nixvim/config/utils/neotest.nix
+++ modules/nixvim/config/utils/neotest.nix
@@ -27,7 +27,6 @@
           summary = {
             enabled = true;
           };
-          consumers = { };
         };
       };
     };
