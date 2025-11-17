4e04e6ae66fe2cc721e26419b7997dd517f774f1
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-11-17 13:44:06 -0800

    chore: rm unecessary change line keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "v", "i" }, "<M-j>", "<Nop>")
+vim.keymap.set({ "n", "v", "i" }, "<M-k>", "<Nop>")
+vim.keymap.set({ "n", "v", "i" }, "<M-h>", "<Nop>")
+vim.keymap.set({ "n", "v", "i" }, "<M-l>", "<Nop>")
