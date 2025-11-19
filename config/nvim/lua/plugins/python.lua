828d6ffe9ca894a30bad89dee257766b44f46737
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-11-18 21:05:49 -0800

    feat(ipython): add ipython compatible cells in py files

--- config/nvim/lua/plugins/python.lua
+++ config/nvim/lua/plugins/python.lua
@@ -16,4 +16,23 @@ return {
       },
     },
   },
+  {
+    "jpalardy/vim-slime",
+    init = function()
+      vim.g.slime_target = "tmux"
+      vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+      vim.g.slime_dont_ask_default = 1
+    end,
+  },
+  {
+    "hanschen/vim-ipython-cell",
+    dependencies = { "jpalardy/vim-slime" },
+    ft = "python",
+    keys = {
+      { "<leader>xr", "<cmd>IPythonCellExecuteCell<cr>", desc = "Execute Cell" },
+      { "<leader>xj", "<cmd>IPythonCellExecuteCellJump<cr>", desc = "Execute Cell & Jump" },
+      { "[c", "<cmd>IPythonCellPrevCell<cr>", desc = "Previous Cell" },
+      { "]c", "<cmd>IPythonCellNextCell<cr>", desc = "Next Cell" },
+    },
+  },
 }
