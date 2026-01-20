b00abe7163c97fd13b1594f627abbf95344674dd
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2026-01-20 19:50:18 +0100

    feat(darwin): add askpass script for sudo GUI prompts

--- users/romaingrx/home-manager-darwin.nix
+++ users/romaingrx/home-manager-darwin.nix
@@ -4,8 +4,18 @@
   config,
   ...
 }:
+let
+  askpass = pkgs.writeShellScriptBin "askpass" ''
+    /usr/bin/osascript -e 'Tell application "System Events" to display dialog "'"$1"'" default answer "" with hidden answer' -e 'text returned of result'
+  '';
+in
 (lib.mkIf pkgs.stdenv.isDarwin {
+  home.packages = [ askpass ];
+
+  home.sessionVariables = {
+    SUDO_ASKPASS = "${askpass}/bin/askpass";
+  };
+
   home.file.".config/sketchybar".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/sketchybar";
-
 })
